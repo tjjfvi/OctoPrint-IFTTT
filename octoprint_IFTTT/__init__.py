6f60cbe42877537f07361701948a8829570a0e97
tjjfvi <tjjfvi@tjjfvi.com>
2018-12-28 01:12:12 +0000

    initial commit

---
+++ octoprint_IFTTT/__init__.py
@@ -0,0 +1,95 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint
+import requests
+
+class IFTTTplugin(
+	octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.AssetPlugin,
+    octoprint.plugin.TemplatePlugin,
+	octoprint.plugin.EventHandlerPlugin,
+):
+
+	def on_event(self, event_name, payload):
+		events = self._settings.get(["events"])
+		default_prefixes = self._settings.get(["default_prefixes"])
+		makerkeys = self._settings.get(["makerkeys"])
+
+
+		for event in events:
+			if event["event_name"] != event_name: continue
+
+			trigger_names = event["trigger_names"]
+
+			if not len(trigger_names):
+				trigger_names = [prefix + event_name for prefix in default_prefixes]
+
+			values = [self._interpret_value(payload, value) for value in event["values"]]
+			payload = { "value1": values[0], "value2": values[1], "value3": values[2] }
+
+			for trigger_name in trigger_names:
+				for makerkey in makerkeys:
+					url = "https://maker.ifttt.com/trigger/%s/with/key/%s" % (trigger_name, makerkey)
+					self._logger.info("sending a request to url: %s, payload: %s" % (url.replace(makerkey, "[REDACTED; %s...]" % makerkey[:8]), payload));
+
+					response = requests.post(url, json=payload)
+					self._logger.info("response: " + response.text)
+
+	def _interpret_value(self, payload, value):
+		if len(value) == 0:
+			return ""
+		if value[0] == ".":
+			return payload[value[1:]]
+		if value[0] == ":":
+			return "." + value[1:]
+		return value
+
+	def get_settings_defaults(self):
+		return dict(
+			makerkeys=[],
+			default_prefixes = ["OctoPrint-", "op-"],
+			events=[
+				dict(event_name="PrintDone", trigger_names=[], values=[".name", ".time", ".origin"]),
+				dict(event_name="PrintStarted", trigger_names=[], values=[".name", ".origin", ""]),
+				dict(event_name="PrintFailed", trigger_names=[], values=[".name", ".reason", ".origin"]),
+			],
+		)
+
+	def get_settings_restricted_paths(self):
+		return dict(admin=[["makerkeys"]])
+
+	def get_template_configs(self):
+		return [dict(type="settings", custom_bindings=True)]
+
+	def get_assets(self):
+		return dict(
+			js=["js/IFTTT.js"],
+			css=["css/IFTTT.css"],
+		)
+
+	def get_update_information(self):
+		return dict(
+			IFTTT=dict(
+				displayName="OctoPrint-IFTTT",
+				displayVersion=self._plugin_version,
+
+				type="github_release",
+				user="tjjfvi",
+				repo="OctoPrint-IFTTT",
+				current=self._plugin_version,
+
+				pip="https://github.com/tjjfvi/OctoPrint-IFTTT/archive/{target_version}.zip"
+			)
+		)
+
+__plugin_name__ = "OctoPrint-IFTTT"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = IFTTTplugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
---
+++ octoprint_IFTTT/static/js/IFTTT.js
@@ -0,0 +1,62 @@
+$(function() {
+    function IFTTTviewModel(parameters) {
+        let self = this, root = self;
+
+		self.startupComplete = ko.observable(false);
+
+		self.makerkeys = ko.computed({
+			read: () => self.startupComplete() && self.settings.makerkeys().join("\n"),
+			write: mks => self.startupComplete() && self.settings.makerkeys(mks.split("\n")),
+		});
+
+		self.defaultPrefixes = ko.computed({
+			read: () => self.startupComplete() && self.settings.default_prefixes().join("\n"),
+			write: dps => self.startupComplete() && self.settings.default_prefixes(dps.split("\n")),
+		});
+
+		self.events = ko.computed(() =>
+			self.startupComplete() ? self.settings.events() : []
+		);
+
+		self.newEvent = () =>
+			self.settings.events.push({
+				event_name: ko.observable(''),
+				trigger_names: ko.observable([]),
+				values: ko.observable([])
+			})
+
+		self.onStartupComplete = () => {
+			self.settings = parameters[0].settings.plugins.IFTTT;
+			self.startupComplete(true);
+			// console.log(self.settings.events(), self.events());
+		}
+
+		self.Event = function(event){
+			let self = this;
+
+			self.name = event.event_name;
+			self.placeholder = ko.computed(() => {
+				console.log(root.settings.default_prefixes(), event);
+				return root.startupComplete() ?
+					root.settings.default_prefixes().map(prefix => prefix + self.name()).join("\n") :
+					""
+			});
+			self.triggerNames = ko.computed({
+				read: () => root.startupComplete() && event.trigger_names().join("\n"),
+				write: mks => root.startupComplete() && event.trigger_names(mks.split("\n")),
+			});
+			console.log(event);
+			[...[,,,]].map((_, i) => self["value" + (i+1)] = ko.computed({
+				read: () => event.values()[i],
+				write: val => event.values(Object.assign(event.values().slice(), { [i]: val })),
+			}));
+			self.remove = () => root.settings.events.remove(event);
+		}
+    }
+
+    OCTOPRINT_VIEWMODELS.push({
+        construct: IFTTTviewModel,
+        dependencies: ["settingsViewModel"],
+        elements: ["#settings_plugin_IFTTT"],
+    });
+});
