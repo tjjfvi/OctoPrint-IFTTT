b4ae61f526fd8cfadc9428eb2ca0efa497701454
tjjfvi <tjjfvi@tjjfvi.com>
2019-01-05 16:46:27 +0000

    added data-uri-ing functionality

--- octoprint_IFTTT/__init__.py
+++ octoprint_IFTTT/__init__.py
@@ -3,13 +3,17 @@ from __future__ import absolute_import
 
 import octoprint
 import requests
+from datauri import DataURI
 
 class IFTTTplugin(
+	octoprint.plugin.StartupPlugin,
 	octoprint.plugin.SettingsPlugin,
     octoprint.plugin.AssetPlugin,
     octoprint.plugin.TemplatePlugin,
 	octoprint.plugin.EventHandlerPlugin,
 ):
+	def on_after_startup(self):
+		self._storage_interface = self._file_manager._storage("local")
 
 	def on_event(self, event_name, payload):
 		events = self._settings.get(["events"])
@@ -26,7 +30,6 @@ class IFTTTplugin(
 				trigger_names = [prefix + event_name for prefix in default_prefixes]
 
 			values = [self._interpret_value(payload, value) for value in event["values"]]
-			values = values + [""] * (3 - len(values))
 			payload = { "value1": values[0], "value2": values[1], "value3": values[2] }
 
 			for trigger_name in trigger_names:
@@ -38,12 +41,18 @@ class IFTTTplugin(
 					self._logger.info("response: " + response.text)
 
 	def _interpret_value(self, payload, value):
+		self._logger.info(value)
 		if len(value) == 0:
 			return ""
 		if value[0] == ".":
 			return payload[value[1:]]
 		if value[0] == ":":
-			return "." + value[1:]
+			return value[1:]
+		if value[0] == "@":
+			path = self._interpret_value(payload, value[(2 if value[1] in "-f" else 1):])
+			if value[1] == "f":
+				path = self._storage_interface.path_on_disk(path)
+			return DataURI.from_file(path)
 		return value
 
 	def get_settings_defaults(self):
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/tjjfvi/OctoPrint-IFTTT"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["python-datauri-0.2.3"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
